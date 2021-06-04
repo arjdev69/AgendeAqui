557a597c4dec4e927eee2f31e7d3eb9e3c79ec5e
Bruno Araujo <brunoa.silva@solutis.com.br>
2021-06-03 22:19:29 -0300

    activity schedules inittial

--- app/src/main/java/com/brunoaraujo/agendeaqui/activities/SchedulesActivity.java
+++ app/src/main/java/com/brunoaraujo/agendeaqui/activities/SchedulesActivity.java
@@ -1,6 +1,7 @@
 package com.brunoaraujo.agendeaqui.activities;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +13,10 @@ public class SchedulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedules);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(SchedulesActivity.this,"Usuário logado...", Toast.LENGTH_SHORT).show();
     }
 }
