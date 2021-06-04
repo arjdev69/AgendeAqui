8b9c6beadc6f3a0654dfc287e7c793b14bce3b89
Bruno Araujo <brunoa.silva@solutis.com.br>
2021-06-03 23:15:17 -0300

    fix user model

--- app/src/main/java/com/brunoaraujo/agendeaqui/model/User.java
+++ app/src/main/java/com/brunoaraujo/agendeaqui/model/User.java
@@ -39,4 +39,12 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Boolean getProvider() {
+        return provider;
+    }
 }
